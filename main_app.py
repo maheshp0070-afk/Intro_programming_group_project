ba5dd0d96a38d3f6f9a3aa3a329bf743ebf2e5f8
Yong Whye Ng <yongwhye@gmail.com>
2025-11-15 15:27:34 +0000

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import logging
+from Adminpage import adminpage
+from login_page import loginpage
+
+logging.basicConfig(level = logging.DEBUG,
+                    filename = 'logs/CampTrack.log',
+                    filemode = 'a',
+                    format='%(module)s - %(levelname)s - %(message)s')
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("CampTrack")
+        self.state('zoomed')
+        self.container = tk.Frame(self)
+        self.container.grid()
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+
+        for F in (loginpage, adminpage):
+            frame = F(self.container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column =0,  sticky='nsew')
+
+        self.show_frame(loginpage)
+
+    def show_frame(self, page_ident):
+        self.frames[page_ident].tkraise()
+
+root = App()
+root.mainloop()
