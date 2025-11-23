6101522d121549ec8f7a5d03c205c24c58110e79
agustyaiyer1 <agustyaiyer1@gmail.com>
2025-11-23 01:43:12 +0000

    Add files via upload
    Added allocations function to Scout leaders

---
+++ allocations.py
@@ -0,0 +1,28 @@
+from persistence import load_json, save_json
+
+ALLOCATIONS_FILE = 'allocations.json'
+
+class AllocationService: 
+
+    def __init__(self):
+        self.allocations = load_json(ALLOCATIONS_FILE) or []
+
+    def save(self):
+        save_json(ALLOCATIONS_FILE, self.allocations)
+
+    def get_camps_for_leader(self, leader_username):
+        return [a["camp_id"] for a in self.allocations if a["leader"] == leader_username]
+
+    def assign_camp_to_leader(self, leader_username, camp_id):
+        for a in self.allocations:
+            if a["leader"] == leader_username and a["camp_id"] == camp_id:
+                return  # already assigned
+        self.allocations.append({"leader": leader_username, "camp_id": camp_id})
+        self.save()
+
+    def remove_camp_from_leader(self, leader_username, camp_id):
+        self.allocations = [
+            a for a in self.allocations
+            if not (a["leader"] == leader_username and a["camp_id"] == camp_id)
+        ]
+        self.save()
--- camps.py
+++ camps.py
@@ -1,10 +1,45 @@
-import csv
+from persistence import load_json, save_json
 
+CAMPS_FILE = 'data/camps.json'
+ALLOCATIONS_FILE = 'data/allocations.json'
 
 class Camp:
-    def __init__(self, camp):
-        self.camp = camp
+    def __init__(self, camp_id, name, location, start_date, end_date, camp_type, food_units_per_day=0):
+        self.camp_id = camp_id      
+        self.name = name
+        self.location = location
+        self.start_date = start_date  
+        self.end_date = end_date
+        self.camp_type = camp_type    
+        self.food_units_per_day = food_units_per_day
 
-    def load_camps_csv(self, path):
-        with open(path) as f:
-            reader = csv.DictReader(f)
+    def to_dict(self):
+        return {
+            "camp_id": self.camp_id,
+            "name": self.name,
+            "location": self.location,
+            "start_date": self.start_date,
+            "end_date": self.end_date,
+            "camp_type": self.camp_type,
+            "food_units_per_day": self.food_units_per_day,
+        }
+    
+    @staticmethod
+    def from_dict(data):
+        return Camp(
+            camp_id=data["camp_id"],
+            name=data["name"],
+            location=data["location"],
+            start_date=data["start_date"],
+            end_date=data["end_date"],
+            camp_type=data["camp_type"],
+            food_units_per_day=data.get("food_units_per_day", 0),
+        )
+    
+def load_all_camps():
+    data = load_json(CAMPS_FILE) or {}
+    return {cid: Camp.from_dict(camp_dict) for cid, camp_dict in data.items()}
+
+def save_all_camps(camps_dict):
+    data = {cid: camp.to_dict() for cid, camp in camps_dict.items()}
+    save_json(CAMPS_FILE, data)
