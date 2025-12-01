207469965a46f2fadea0f9994d9e2a9a5f1f2434
Mahesh Pasapula <maheshp0070@googlemail.com>
2025-12-01 01:04:52 +0000

    Just better bro 2

--- Log_coord_and_init.py
+++ Log_coord_and_init.py
@@ -3,11 +3,11 @@ import datetime
 
 class Camp:
 
-   def __init__(self, name, location, type, start_date, end_date, food_supply_per_day, food_demand_per_day, leader, pay):
+    def __init__(self, name, location, camp_type, start_date, end_date, food_supply_per_day, food_demand_per_day, leader, pay):
 
        self.name = name
        self.location = location
-       self.type = type
+       self.camp_type = camp_type
        self.start_date = start_date
        self.end_date = end_date
        self.food_supply_per_day = food_supply_per_day
@@ -15,10 +15,11 @@ class Camp:
        self.leader = leader
        self.pay = pay
 
-   @classmethod
-   def load_camps (cls, file):
+    @classmethod
+    def load_camps (cls, file):
 
        df_camps = pd.read_csv(file)
+       global camps
        camps = {}
 
        for index, row in df_camps.iterrows():
@@ -26,7 +27,7 @@ class Camp:
            camp = Camp(
                name = row['name'],
                location = row['location'],
-               type = row['type'],
+               camp_type = row['camp_type'],
                start_date = datetime.datetime.strptime(row['start_date'], '%d-%m-%Y'),
                end_date = datetime.datetime.strptime(row['end_date'], '%d-%m-%Y'),
                food_supply_per_day = int(row['food_supply_per_day']),
@@ -39,14 +40,35 @@ class Camp:
 
        return camps
 
-   def get_campers(self, campers_file):
+    def get_campers(self, campers_file):
 
        df_campers = pd.read_csv(campers_file)
 
-       campers = [row['camper_id'] for index, row in df_campers.iterrows() if type(row['camps']) == str if self.name in row['camps'].split(',')]
+       campers = [row['camper_id'] for index, row in df_campers.iterrows() if isinstance(row['camps'], str) and self.name in row['camps'].split(',')]
 
        return campers
-   
+
+    def save_camps(file):
+
+       camp_data = []
+
+       for camp in camps.values():
+
+           camp_data.append({
+               'name': camp.name,
+               'location': camp.location,
+               'camp_type': camp.camp_type,
+               'start_date': camp.start_date.strftime('%d-%m-%Y'),
+               'end_date': camp.end_date.strftime('%d-%m-%Y'),
+               'food_supply_per_day': camp.food_supply_per_day,
+               'food_demand_per_day': camp.food_demand_per_day,
+               'leader': camp.leader,
+               'pay': round(float(camp.pay), 2)
+           })
+
+       df_camps = pd.DataFrame(camp_data)
+       df_camps.to_csv(file, index=False)
+
 class User:
 
     def __init__ (self, username, password, role, status):
@@ -64,58 +86,93 @@ class User:
 
         for index, row in df_users.iterrows():
 
-            user = User(
-                username = row["username"],
-                password = row["password"],
-                role = row["role"],
-                status = row["status"]
-            )
+            if row["role"] == cls.__name__.lower():
             
-            users[user.username] = user
+                user = cls(
+                    username = row["username"],
+                    password = row["password"],
+                    status = row["status"]
+                )
+            
+                users[user.username] = user
         
         return users
     
-    def get_camps(self, camps_dict):
+    @classmethod
+    def load_all_users(cls, file):
 
-        if self.role == "leader":
+        df_users = pd.read_csv(file)
+        global all_users
+        all_users = {}
 
-            camps = [camp for camp in camps_dict if camp.leader == self.username]
-            return camps
+        for index, row in df_users.iterrows():
+
+            if row["role"] == 'admin':
+                user = Admin(
+                    username = row["username"],
+                    password = row["password"],
+                    status = row["status"]
+                )
+            elif row["role"] == 'coordinator':
+                user = Coordinator(
+                    username = row["username"],
+                    password = row["password"],
+                    status = row["status"]
+                )
+            elif row["role"] == 'leader':
+                user = Leader(
+                    username = row["username"],
+                    password = row["password"],
+                    status = row["status"]
+                )
+            
+            all_users[user.username] = user
         
-    def create_camp(self, camps_dict, name, location, type, start_date, end_date, food_supply_per_day, leader, pay):
+        return all_users
 
-        if self.role == "coordinator":
+class Admin(User):
 
-            new_camp = Camp(
-                name = name,
-                location = location,
-                type = type,
-                start_date = datetime.datetime.strptime(start_date, '%d-%m-%Y'),#needs changing depending on tkinter
-                end_date = datetime.datetime.strptime(end_date, '%d-%m-%Y'),
-                food_supply_per_day = int(food_supply_per_day),
-                food_demand_per_day = 0,
-                leader = leader,
-                pay = round(float(pay), 2)
-            )
+    def __init__(self, username, password, status):
 
-            camps_dict[new_camp.name] = new_camp
+        super().__init__(username, password, 'admin', status)
+
+class Coordinator(User):
+
+    def __init__(self, username, password, status):
+
+        super().__init__(username, password, 'coordinator', status)
+
+    def create_camp(self, camps_dict, name, location, camp_type, start_date, end_date, food_supply_per_day, pay):
+
+        new_camp = Camp(
+            name = name,
+            location = location,
+            camp_type = camp_type,
+            start_date = start_date,
+            end_date = end_date,
+            food_supply_per_day = int(food_supply_per_day),
+            food_demand_per_day = 0,
+            leader = "unassigned",
+            pay = round(float(pay), 2)
+        )
+
+        camps_dict[new_camp.name] = new_camp
     
-    def topup(self, camp, amount):
+    def set_food(self, camp, amount):
         
-        if self.role == "coordinator":
-            camp.food_supply_per_day += amount
+        camp.food_supply_per_day = amount
 
     def update_pay(self, camp, new_pay):
 
-        if self.role == "coordinator":
-            camp.pay = round(float(new_pay), 2)
+        camp.pay = round(float(new_pay), 2)
     
+class Leader(User):
 
+    def __init__(self, username, password, status):
 
-    
-            
-        
+        super().__init__(username, password, 'leader', status)
 
-                
-    
+    def get_camps(self, camps_dict):
 
+        camps = [camp for camp in camps_dict.values() if camp.leader == self.username]
+        return camps
