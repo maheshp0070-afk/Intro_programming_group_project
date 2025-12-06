2259d3a8e4e5021c2e3d2407a9b609909a71d418
Yong Whye Ng <yongwhye@gmail.com>
2025-12-06 01:13:47 +0000

    Refactor LoginPage class and add input validation
    Refactor LoginPage to remove controller parameter and add validation for username and password entries.

--- login_page.py
+++ login_page.py
@@ -5,9 +5,8 @@ from admin_GUI import AdminPage
 from admin_logic import *
 
 class LoginPage(tk.Frame):
-    def __init__(self, App, controller):
+    def __init__(self, App):
         super().__init__(App)
-        self.controller = controller
         self.canvas = tk.Canvas(self, width = 1280, height = 720)
         self.canvas.pack(expand=True, fill="both")
 
@@ -21,8 +20,8 @@ class LoginPage(tk.Frame):
         p_label = tk.Label(self.canvas, text="Enter your password: ", font = ("Comic Sans MS", 20), bg= "white")
         login_button = ttk.Button(self.canvas, text="Login", command=self.login_)
 
-        self.login = ttk.Entry(self.canvas, width=40, font = ("Comic Sans MS", 20))
-        self.password = ttk.Entry(self.canvas, width=40, show="*", font = ("Comic Sans MS", 20))
+        self.login = ttk.Entry(self.canvas, width=40, font = ("Comic Sans MS", 20), validate="key", validatecommand = (self.register(lambda x: len(x)<20), '%P'))
+        self.password = ttk.Entry(self.canvas, width=40, show="*", font = ("Comic Sans MS", 20), validate="key", validatecommand = (self.register(lambda x: len(x)<20), '%P'))
 
         self.canvas.create_window(640, 200, window = title_label)
         self.canvas.create_window(640, 250, window = u_label)
@@ -37,25 +36,38 @@ class LoginPage(tk.Frame):
         password = self.password.get()
         users = UserManager.load_users()
 
+
         #Check username and password is in database
-        if username not in users["username"].values or users.loc[username, "password"] != password :
+        if username not in users["username"].values:
             messagebox.showerror("Error", "Incorrect username or password")
             return
 
-        role = users.loc[username, "role"]
+        user_row = users[users["username"] == username].iloc[0]
+        role = user_row["role"]
+        status = user_row["status"]
 
         if role == "admin":
             logging.info("Admin Logged In")
-            self.controller.show_frame(AdminPage)
-        if role == "Coordinator":
-            logging.info("Logistics Coordinator Logged In")
-            pass
+            self.destroy()
+            admin_window = AdminPage(self.master)
+            admin_window.pack(fill = "both", expand = True)
+        if status == True:
+            if role == "coordinator":
+                logging.info("Logistics Coordinator Logged In")
+                self.destroy()
+                from ah import CoordinatorDashboard
+                log_window = CoordinatorDashboard(self.master)
+                log_window.pack(fill = "both", expand = True)
+            if role == "leader":
+                logging.info(f"{self.username} Logged In")
+                pass
+        elif status == False:
+            messagebox.showerror("Error", "User is disabled.")
 
 if __name__ == '__main__':
     root = tk.Tk()
-    app = LoginPage(root, root)
+    app = LoginPage(root)
     root.geometry("1280x720")
     root.resizable(False, False)
     app.pack(fill = "both", expand = True)
-    root.mainloop()
     app.mainloop()
