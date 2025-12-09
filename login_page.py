04eb65ef53e870923428a25a453a087474c2c31c
Yong Whye Ng <yongwhye@gmail.com>
2025-12-09 12:12:06 +0000

    Update login page
    Refactor LoginPage to remove controller dependency and enhance UI with a canvas and background image.

--- login_page.py
+++ login_page.py
@@ -5,41 +5,68 @@ from admin_GUI import AdminPage
 from admin_logic import *
 
 class LoginPage(tk.Frame):
-    def __init__(self, App, controller):
+    def __init__(self, App):
         super().__init__(App)
-        self.controller = controller
-        ttk.Label(self, text = "CampTrack Login Page", font = ("Arial", 20)).pack()
-        ttk.Label(self, text = "Enter your login: ").pack()
-        self.login = ttk.Entry(self, width = 40)
-        self.login.pack()
+        self.canvas = tk.Canvas(self, width = 1280, height = 720)
+        self.canvas.pack(expand=True, fill="both")
 
-        ttk.Label(self, text = "Enter your password: ").pack()
-        self.password = ttk.Entry(self, width = 40, show = "*")
-        self.password.pack()
+        # Adds Background Photo
+        self.bg = tk.PhotoImage(file="Desert.png")
+        self.canvas_bg = self.canvas.create_image(0, 0, image=self.bg, anchor="nw")
 
-        ttk.Button(self, text="Login", command=self.login_).pack()
+        #Username and Password
+        title_label = tk.Label(self, text = "CampTrack Login Page", font = ("Comic Sans MS", 20), bg= "white")
+        u_label = tk.Label(self.canvas, text = "Enter your login: ", font = ("Comic Sans MS", 20), bg= "white")
+        p_label = tk.Label(self.canvas, text="Enter your password: ", font = ("Comic Sans MS", 20), bg= "white")
+        login_button = ttk.Button(self.canvas, text="Login", command=self.login_)
+
+        self.login = ttk.Entry(self.canvas, width=40, font = ("Comic Sans MS", 20), validate="key", validatecommand = (self.register(lambda x: len(x)<20), '%P'))
+        self.password = ttk.Entry(self.canvas, width=40, show="*", font = ("Comic Sans MS", 20), validate="key", validatecommand = (self.register(lambda x: len(x)<20), '%P'))
+
+        self.canvas.create_window(640, 200, window = title_label)
+        self.canvas.create_window(640, 250, window = u_label)
+        self.canvas.create_window(640, 340, window = p_label)
+        self.canvas.create_window(640, 290, window = self.login)
+        self.canvas.create_window(640, 380, window = self.password)
+        self.canvas.create_window(640, 420, window = login_button)
+
 
     def login_(self):
         username = self.login.get()
         password = self.password.get()
         users = UserManager.load_users()
 
+
         #Check username and password is in database
-        if username not in users["username"].values or users.loc[username, "password"] != password :
+        if username not in users["username"].values:
             messagebox.showerror("Error", "Incorrect username or password")
             return
 
-        role = users.loc[username, "role"]
+        user_row = users[users["username"] == username].iloc[0]
+        role = user_row["role"]
+        status = user_row["status"]
 
         if role == "admin":
             logging.info("Admin Logged In")
-            self.controller.show_frame(AdminPage)
-        if role == "Coordinator":
-            logging.info("Logistics Coordinator Logged In")
-            pass
+            self.destroy()
+            admin_window = AdminPage(self.master)
+            admin_window.pack(fill = "both", expand = True)
+        if status == True:
+            if role == "coordinator":
+                logging.info("Logistics Coordinator Logged In")
+                self.destroy()
+                import CoordinatorDashboard
+                pass
+            if role == "leader":
+                logging.info(f"{self.username} Logged In")
+                pass
+        elif status == False:
+            messagebox.showerror("Error", "User is disabled.")
 
 if __name__ == '__main__':
     root = tk.Tk()
-    app = LoginPage(root, root)
-    app.pack()
+    app = LoginPage(root)
+    root.geometry("1280x720")
+    root.resizable(False, False)
+    app.pack(fill = "both", expand = True)
     app.mainloop()
